0b6bcb13d662811892e036cc57da4a510f56b267
YLHplus <93420580+luumod@users.noreply.github.com>
2022-06-08 23:28:35 +0800

    Add files via upload

---
+++ 2练习题.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//1
+//int main(void)
+//{
+//	string name;
+//	string address;
+//	cout << "请输入名字:";
+//	cin >> name;
+//	cout << "请输入地址:";
+//	cin >> address;
+//	cout << "你的名字是:" << name << endl;
+//	cout << "你的地址是:" << address << endl;
+//	return 0;
+//}
+
+//2
+//int LONG(int a);
+//int main(void)
+//{
+//	int Long;
+//	cout << "请输入long:";
+//	cin >> Long;
+//	cout << "你的long为:" << LONG(Long) << endl;
+//
+//	return 0;
+//}
+//int LONG(int a)
+//{
+//	return a * 220;
+//}
+
+
+//3
+//void str1()
+//{
+//	cout << "Three blind mice" << endl;
+//	cout << "Three blind mice" << endl;
+//}
+//void str2()
+//{
+//	cout << "See how they run"<<endl;
+//	cout << "See how they run"<<endl;
+//}
+//int main(void)
+//{
+//	str1();
+//	str2();
+//
+//	return 0;
+//}
+
+
+//4
+//int main(void)
+//{
+//	int age;
+//	cout << "请输入你的年龄:";
+//	cin >> age;
+//	cout << "你的年龄包含" << age * 12 << "个月" << endl;
+//	return 0;
+//}
+
+
+//5
+//double temp(double n)
+//{
+//	return 1.8 * n + 32.0;
+//}
+//int main()
+//{
+//	int nm;
+//	cout << "Please enter a Celsius value:";
+//	cin >> nm;
+//	cout << nm << " degrees Celsius is " << temp(nm) << " degrees Fahrenheit." << endl;
+//	return 0;
+//}
+
+
+//6.
+//double temp(double n)
+//{
+//	return n * 63240;
+//}
+//int main()
+//{
+//	double nm;
+//	cout << "Enter the number of light year:";
+//	cin >> nm;
+//	cout << nm << " light years= " << temp(nm) << " astronmical units." << endl;
+//	return 0;
+//}
+
+
+//7
+//void show(int a, int b)
+//{
+//	cout << "Time: " << a << ":" << b << endl;
+//}
+//int main(void)
+//{
+//	int hour, min;
+//	cout << "Enter the number of hour:";
+//	cin >> hour;
+//	cout << "Enter the number of minutes:";
+//	cin >> min;
+//	show(hour, min);
+//	return 0;
+//}
